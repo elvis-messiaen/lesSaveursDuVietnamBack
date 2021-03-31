5b8ff58de479224a68be6ca1a4d81ea1372764dc
elvis <elvism72@hotmail.com>
2021-03-31 19:13:03 +0200

    first commit

---
+++ src/main/java/ecommerce/LesSaveursDuVietnamApplication.java
@@ -0,0 +1,13 @@
+package ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesSaveursDuVietnamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesSaveursDuVietnamApplication.class, args);
+	}
+
+}
